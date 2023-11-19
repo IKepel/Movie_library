9aa669985045c964bb643a159e36ec639b371898
Kostya Popov <kepel2823@gmail.com>
2023-11-19 18:59:08 +0200

    Add project files.

---
+++ Movie_library/MovieLibrary.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_library
+{
+    public class MovieLibrary : IEnumerable
+    {
+        private Dictionary<int, string> _ordinaryMovies = new Dictionary<int, string>
+        {
+            { 1, "Harry Potter" },
+            { 2, "The Lord of the Rings" },
+            { 3, "Transformers" }
+        };
+
+        private Dictionary<int, string> _onlyAdultMovies = new Dictionary<int, string>
+        {
+            { 101, "Gladiator" },
+            { 102, "Piranha 3D" },
+            { 103, "Room in Rome" }
+        };
+
+        public string this[int movieNum]
+        {
+            get
+            {
+                if (IsNightTime())
+                {
+                    if (_onlyAdultMovies.ContainsKey(movieNum))
+                        return _onlyAdultMovies[movieNum];
+                    if (_ordinaryMovies.ContainsKey(movieNum))
+                        return _ordinaryMovies[movieNum];
+                }
+                else
+                {
+                    if (_ordinaryMovies.ContainsKey(movieNum))
+                        return _ordinaryMovies[movieNum];
+                }
+
+                throw new KeyNotFoundException("Movie not found");
+            }
+        }
+
+        public string GetMovie(int movieNum)
+        {
+            try
+            {
+                return this[movieNum];
+            }
+            catch (KeyNotFoundException)
+            {
+                return "Movie not found";
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            if (IsNightTime())
+            {
+                foreach (var movie in _ordinaryMovies.Values)
+                {
+                    yield return movie;
+                }
+                foreach (var movie in _onlyAdultMovies.Values)
+                {
+                    yield return movie;
+                }
+            }
+            else
+            {
+                foreach (var movie in _ordinaryMovies.Values)
+                {
+                    yield return movie;
+                }
+            }
+        }
+
+        private bool IsNightTime()
+        {
+            var now = DateTime.Now;
+            return now.Hour >= 23 || now.Hour < 7;
+        }
+    }
+}
---
+++ Movie_library/Program.cs
@@ -0,0 +1,10 @@
+﻿using Movie_library;
+
+var movieLibrary = new MovieLibrary();
+
+foreach (var movie in movieLibrary)
+{
+    Console.WriteLine(movie);
+}
+
+Console.WriteLine($"\n{movieLibrary.GetMovie(2)}"); 
